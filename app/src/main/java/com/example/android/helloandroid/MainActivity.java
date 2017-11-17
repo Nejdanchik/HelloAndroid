b8b7befec91c9c507d20896dc5ad26027a6bfae7
Nejdanchik <alexeigt92@gmail.com>
2017-11-17 18:38:18 +0300

    Initial commit with my project

---
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView site = (TextView)findViewById(R.id.site);
+        TextView tel = (TextView)findViewById(R.id.tel);
+
+
+        site.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri address = Uri.parse("https://www.udacity.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,address);
+                startActivity(intent);
+            }
+        });
+
+        tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:6505555555"));
+                startActivity(intent1);
+            }
+        });
+
+    }
+}
